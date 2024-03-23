175562c3533308e6051e23222dcbc4e112e72447
Kincaid <kiprotichkincaid@gmail.com>
2024-03-23 11:27:42 +0300

    added a splider section

--- eyesharknews/src/Pages/Home.jsx
+++ eyesharknews/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from './Homepages/hero.jsx';
 import Business from './Homepages/business.jsx';
 import Latest from './Homepages/latest.jsx';
+import World from './Homepages/world.jsx';
 
 const Home = () => {
   return (
     <div>
         <Hero />
         <Business />
+        <World />
         <Latest />
     </div>
   )
